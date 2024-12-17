3ae74869346aab655ef5baf04dc8dc2a32fcb61e
Brown, Shaun <shaun.brown@mail.house.gov>
2024-12-17 16:00:42 +0000

    Init commit

---
+++ house.goggle.go
@@ -0,0 +1,49 @@
+# House.gov Websites Goggle
+! name: House.gov Websites
+! description: Official U.S. House of Representatives websites and Congress.gov, with priority weighting
+! public: false
+! author: Custom Goggle
+! avatar: #USA
+
+# Primary domains (boost=4)
+! boost=4,site=house.gov
+! boost=4,site=congress.gov
+
+# Committee sites (boost=3)
+! boost=3,site=rules.house.gov
+! boost=3,site=ethics.house.gov
+! boost=3,site=appropriations.house.gov
+! boost=3,site=budget.house.gov
+! boost=3,site=judiciary.house.gov
+! boost=3,site=agriculture.house.gov
+! boost=3,site=intelligence.house.gov
+! boost=3,site=armedservices.house.gov
+! boost=3,site=education.house.gov
+! boost=3,site=energycommerce.house.gov
+! boost=3,site=financialservices.house.gov
+! boost=3,site=foreignaffairs.house.gov
+! boost=3,site=homeland.house.gov
+! boost=3,site=transportation.house.gov
+! boost=3,site=veterans.house.gov
+! boost=3,site=naturalresources.house.gov
+! boost=3,site=oversight.house.gov
+! boost=3,site=science.house.gov
+! boost=3,site=smallbusiness.house.gov
+! boost=3,site=taxreform.house.gov
+! boost=3,site=waysandmeans.house.gov
+
+# Officer and administrative sites (boost=3)
+! boost=3,site=clerk.house.gov
+! boost=3,site=docs.house.gov
+! boost=3,site=history.house.gov
+! boost=3,site=cao.house.gov
+! boost=3,site=ogc.house.gov
+! boost=3,site=sergeant.house.gov
+! boost=3,site=oig.house.gov
+! boost=3,site=chaplain.house.gov
+
+# Member sites (boost=2)
+! boost=2,site=*.house.gov
+
+# Exclude all other sites
+! boost=-4,site=*
